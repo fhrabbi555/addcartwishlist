45d7afebfefca6f801157c25b4c1928ddbf7060f
Fuad Hasan Rabbi <fhrabbi555@gmail.com>
2024-02-19 17:54:18 +0600

    bloc updated

--- lib/features/home/bloc/homebloc_bloc.dart
+++ lib/features/home/bloc/homebloc_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -6,6 +8,30 @@ part 'homebloc_state.dart';
 
 class HomeblocBloc extends Bloc<HomeblocEvent, HomeblocState> {
   HomeblocBloc() : super(HomeblocInitial()) {
-    on<HomeProductWishlistButtonClickEvent>(HomePro);
+    on<HomeProductWishlistButtonClickEvent>(
+        homeProductWishlistButtonClickEvent);
+    on<HomeProductCartButtonClickEvent>(homeProductCartButtonClickEvent);
+    on<HomeWishlistButtonNevigateEvent>(homeWishlistButtonNevigateEvent);
+    on<HomecarttButtonNevigateEvent>(homecarttButtonNevigateEvent);
   }
+
+  FutureOr<void> homeProductWishlistButtonClickEvent(
+      HomeProductWishlistButtonClickEvent event, Emitter<HomeblocState> emit) {
+    print('Wishlist Product Clicked');
+  }
+
+  FutureOr<void> homeProductCartButtonClickEvent(
+      HomeProductCartButtonClickEvent event, Emitter<HomeblocState> emit) {
+    print('Cart Product Clicked');
+  }
+}
+
+FutureOr<void> homeWishlistButtonNevigateEvent(
+    HomeWishlistButtonNevigateEvent event, Emitter<HomeblocState> emit) {
+  print('Wishlist Navigate Clicked');
+}
+
+FutureOr<void> homecarttButtonNevigateEvent(
+    HomecarttButtonNevigateEvent event, Emitter<HomeblocState> emit) {
+  print('Cart Navigate Clicked');
 }
