8927816cc65bc391527f59795af8e89c45a7c662
Fuad Hasan Rabbi <fhrabbi555@gmail.com>
2024-02-19 12:47:25 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/addcartwishlist/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.addcartwishlist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,76 @@
+class GroceryData{
+
+  static <Map<String, dynamic>> groceryProducts = [
+  {
+    "id": 1,
+    "name": "Apples",
+    "description": "Fresh, juicy apples",
+    "price": 2.99,
+    "imageUrl": "https://example.com/apples.jpg",
+  },
+  {
+    "id": 2,
+    "name": "Bananas",
+    "description": "Ripe, yellow bananas",
+    "price": 1.99,
+    "imageUrl": "https://example.com/bananas.jpg",
+  },
+  {
+    "id": 3,
+    "name": "Milk",
+    "description": "Whole milk, 1 gallon",
+    "price": 3.49,
+    "imageUrl": "https://example.com/milk.jpg",
+  },
+  {
+    "id": 4,
+    "name": "Eggs",
+    "description": "Large brown eggs, dozen",
+    "price": 2.19,
+    "imageUrl": "https://example.com/eggs.jpg",
+  },
+  {
+    "id": 5,
+    "name": "Bread",
+    "description": "Freshly baked bread loaf",
+    "price": 2.79,
+    "imageUrl": "https://example.com/bread.jpg",
+  },
+  {
+    "id": 6,
+    "name": "Chicken",
+    "description": "Boneless chicken breasts, 1lb",
+    "price": 5.99,
+    "imageUrl": "https://example.com/chicken.jpg",
+  },
+  {
+    "id": 7,
+    "name": "Rice",
+    "description": "Long-grain white rice, 5lb",
+    "price": 4.49,
+    "imageUrl": "https://example.com/rice.jpg",
+  },
+  {
+    "id": 8,
+    "name": "Pasta",
+    "description": "Spaghetti pasta, 1lb",
+    "price": 1.99,
+    "imageUrl": "https://example.com/pasta.jpg",
+  },
+  {
+    "id": 9,
+    "name": "Tomatoes",
+    "description": "Vine-ripened tomatoes, 1lb",
+    "price": 2.49,
+    "imageUrl": "https://example.com/tomatoes.jpg",
+  },
+  {
+    "id": 10,
+    "name": "Potatoes",
+    "description": "Russet potatoes, 5lb",
+    "price": 1.79,
+    "imageUrl": "https://example.com/potatoes.jpg",
+  },
+];
+
+}
